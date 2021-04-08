c414c2f7395e6114e72d177799e1b9203201d68a
Vinicius <viniciusfod@gmail.com>
2021-04-08 10:45:29 -0300

    adiciona exercicio 3

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -59,3 +59,23 @@ function createDaysOfTheWeek() {
   }
   createCustomButton('Feriado');
 
+//exercicio 3-
+
+  function colorHoliday () {
+    let getHolidayButton = document.querySelector('#newBtn');
+    let getHolidays = document.querySelectorAll('.holiday');
+    let backgroundColor = 'rgb(238,238,238)';
+    let setNewColor = "white";
+
+    getHolidayButton.addEventListener('click', function () {
+      for (let index = 0; index < getHolidays.length; index += 1) {
+        if (getHolidays[index].style.backgroundColor === setNewColor) {
+          getHolidays[index].style.backgroundColor = backgroundColor;
+        } else {
+          getHolidays[index].style.backgroundColor = setNewColor;
+        }
+      }
+    })
+  }
+
+  colorHoliday();
