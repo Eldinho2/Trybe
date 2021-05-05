8396c835af0deb25a200b3c0988e0aeb0835005a
Vinicius <viniciusfod@gmail.com>
2021-05-05 16:17:34 -0300

    exercicios bloco 8 dia 3

--- bloco_8/dia_3/exercicio7.js
+++ bloco_8/dia_3/exercicio7.js
@@ -63,51 +63,13 @@ const books = [
   },
 ];
 
-const expectedResult = [
- {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
-];
+const expectedResult = 'O Senhor dos Anéis';
 
-function fantasyOrScienceFiction() {
-  return books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
+function authorWith3DotsOnName() {
+	const authorName = ['J. R. R. Tolkien']
+	return books
+	.filter((book) => authorName.includes(book.author.name))
+	.map((book) => book.name)
 }
-
-assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+console.log(authorWith3DotsOnName());
+//assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
