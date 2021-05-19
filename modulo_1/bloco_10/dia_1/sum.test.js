31185f9ef33ef6c186fa7c21dae6d7836aeb8f4a
Vinicius <viniciusfod@gmail.com>
2021-05-19 08:48:23 -0300

    adiciona git ignore e mais testes para funcao sum

--- modulo_1/bloco_10/dia_1/sum.test.js
+++ modulo_1/bloco_10/dia_1/sum.test.js
@@ -2,6 +2,10 @@ const sum = require('./sum.js')
 
 describe('testes sum', () => {
   it('testa se soma de 4 e 5 é igual a 9', () => {
-		expect(sum(4, 5)).toBe(9)
+	  expect(sum(4, 5)).toBe(9)
 	})
+
+  it('teste se 0 + 0 é igial a 0', () => {
+	  expect(sum(0, 0)).toBe(0)
+  })
 })
