1d246f29ab1733c7e2e174d18a3730d943023faa
Vinicius <viniciusfod@gmail.com>
2021-05-19 09:00:38 -0300

    testes para funcao sum

--- modulo_1/bloco_10/dia_1/sum.test.js
+++ modulo_1/bloco_10/dia_1/sum.test.js
@@ -2,10 +2,16 @@ const sum = require('./sum.js')
 
 describe('testes sum', () => {
   it('testa se soma de 4 e 5 é igual a 9', () => {
-	  expect(sum(4, 5)).toBe(9)
+	  expect(sum(4, 5)).toBe(9);
 	})
 
-  it('teste se 0 + 0 é igial a 0', () => {
-	  expect(sum(0, 0)).toBe(0)
+  it('teste se 0 + 0 é igual a 0', () => {
+	  expect(sum(0, 0)).toBe(0);
   })
+
+  it('error message is "parameters must be numbers"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow('parameters must be numbers');
+  });
 })
