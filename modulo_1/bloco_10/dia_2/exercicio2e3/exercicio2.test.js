d3b53ca9a33602e12afb63eae5a7389eaf556152
Vinicius <viniciusfod@gmail.com>
2021-05-20 09:57:07 -0300

    adiciona testes ASYNC/AWAIT para funcao do exercicio2

--- modulo_1/bloco_10/dia_2/exercicio2e3/exercicio2.test.js
+++ modulo_1/bloco_10/dia_2/exercicio2e3/exercicio2.test.js
@@ -11,3 +11,15 @@ describe('testes para funcao getUserName', () => {
 		return getUserName(2).catch(error => expect(error).toEqual({ error: 'User with 2 not found.'}));
 	});
 });
+
+describe('testes para funcao getUserName ASYNC/AWAIT', () => {
+	it('testa se a funcao retorna um id que existe ASYNC/AWAIT', async() => {
+		expect.assertions(1);
+		return await getUserName(4).then(user => expect(user).toEqual('Mark'));
+	});
+
+	it('testa quando o id nao existe ASYNC/AWAIT', async () => {
+		expect.assertions(1);
+		return  await getUserName(2).catch(error => expect(error).toEqual({ error: 'User with 2 not found.'}));
+	});
+});
