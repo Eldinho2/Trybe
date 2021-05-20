a8fecc2b33e3766aaeb647ebefc206deff1a9955
Vinicius <viniciusfod@gmail.com>
2021-05-20 09:53:48 -0300

    adiciona teste 2 para o exercicio 2

--- modulo_1/bloco_10/dia_2/exercicio2e3/exercicio2.test.js
+++ modulo_1/bloco_10/dia_2/exercicio2e3/exercicio2.test.js
@@ -1,8 +1,13 @@
 const { getUserName } = require('./exercicio2');
 
 describe('testes para funcao getUserName', () => {
-	it('testa se a funcao retorna um id que existe', async () => {
+	it('testa se a funcao retorna um id que existe', () => {
 		expect.assertions(1);
 		return getUserName(4).then(user => expect(user).toEqual('Mark'));
+	});
+
+	it('testa quando o id nao existe', () => {
+		expect.assertions(1);
+		return getUserName(2).catch(error => expect(error).toEqual({ error: 'User with 2 not found.'}));
 	});
 });
