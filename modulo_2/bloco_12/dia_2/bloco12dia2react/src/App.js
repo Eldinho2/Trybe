9b5c52537c3d450611cd4b11329b55a9c2e2b1d4
Vinicius <viniciusfod@gmail.com>
2021-06-07 08:46:02 -0300

    form

---
+++ modulo_2/bloco_12/dia_2/bloco12dia2react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Form from './Form'
+import './Form.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="div">Formulario</h1>
+        <Form />
+      </div>
+    )
+  }
+}
+
