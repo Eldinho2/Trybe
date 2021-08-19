836d2cb2218832f95fb1c495b0213b0e88043611
Vinicius <viniciusfod@gmail.com>
2021-08-19 01:10:46 -0300

    exercicios20.2

---
+++ BACK_END/modulo_3/bloco_20/20.2/conteudo/DISTINCT.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE `trybe_20-2_escola`;
+USE `trybe_20-2_escola`;
+CREATE TABLE IF NOT EXISTS Alunos (
+    `Nome` VARCHAR(7) CHARACTER SET utf8,
+    `Idade` INT
+);
+INSERT INTO Alunos VALUES
+    ('Rafael', 25),
+    ('Amanda', 30),
+    ('Roberto', 45),
+    ('Carol', 19),
+    ('Amanda', 25);
+
+
+USE `trybe_20-2_escola`;
+SELECT * FROM Alunos;
+
+SELECT * FROM Alunos WHERE Idade < 20;
+
+SELECT DISTINCT Idade FROM Alunos;
