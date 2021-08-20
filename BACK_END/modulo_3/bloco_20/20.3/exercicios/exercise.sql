f7442a7cf0e5441af605bca9b14b2e4a5cc4cc33
Vinicius <viniciusfod@gmail.com>
2021-08-20 00:48:49 -0300

    exercicios 20.3

---
+++ BACK_END/modulo_3/bloco_20/20.3/exercicios/exercise.sql
@@ -0,0 +1,42 @@
+-- 1
+
+USE PecasFornecedores;
+SELECT * FROM Pecas
+WHERE name LIKE 'GR%';
+
+-- 2
+
+USE PecasFornecedores;
+SELECT * FROM Fornecimentos
+WHERE Peca LIKE 2 ORDER BY Fornecedor;
+
+-- 3
+
+USE PecasFornecedores;
+SELECT peca, preco, fornecedor FROM Fornecimentos
+WHERE Fornecedor LIKE '%N%';
+
+-- 4
+
+USE PecasFornecedores;
+SELECT * FROM Fornecedores
+WHERE name LIKE '%LTDA%' ORDER BY name DESC;
+
+-- 5
+
+USE PecasFornecedores;
+SELECT COUNT(*) FROM Fornecedores
+WHERE code LIKE '%F%'
+
+-- 6
+
+USE PecasFornecedores;
+SELECT * FROM Fornecimentos
+WHERE preco BETWEEN 15 AND 40 ORDER BY preco;
+
+-- 7
+
+USE PecasFornecedores;
+SELECT COUNT(*) FROM Vendas
+WHERE DATE(order_date) BETWEEN '2018-04-15' AND '2019-07-30';
+
